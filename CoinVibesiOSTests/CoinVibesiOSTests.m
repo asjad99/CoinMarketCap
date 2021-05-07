77cd015e829e61a5e97b460118788c1fd0d2adbd
Asjad Khan <asjad99@users.noreply.github.com>
2021-05-07 20:55:46 +1000

    Add files via upload

---
+++ CoinVibesiOSTests/CoinVibesiOSTests.m
@@ -0,0 +1,34 @@
+//
+//  CoinVibesiOSTests.m
+//  CoinVibesiOSTests
+//
+//  Created by Muhammad Asjad on 12/5/13.
+//  Copyright (c) 2013 Muhammad Asjad. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CoinVibesiOSTests : XCTestCase
+
+@end
+
+@implementation CoinVibesiOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
